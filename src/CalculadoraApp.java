7c93d8c30325b445c18c23a72d2227646a31c768
Federico Rossendy <fede07@users.noreply.github.com>
2024-01-17 21:03:32 -0300

    End

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -3,16 +3,65 @@ import java.util.Scanner;
 public class CalculadoraApp {
     
     public static void main(String[] args) {
-        System.out.println("***** Calculator App *****");
-        System.out.print("First value: ");
         Scanner consola = new Scanner(System.in);
-        var operando1 = Integer.parseInt(consola.nextLine());
-        System.out.println("Second value: ");
-        var operando2 = Integer.parseInt(consola.nextLine());
-        var resultado = operando1 + operando2;
-
-        System.out.println("Result: " + resultado);
-
+        boolean app_on = true;
+        while (app_on) {
+            showMenu();
+            try{
+                var operacion = Integer.parseInt(consola.nextLine());
+                if(operacion >= 1 && operacion <= 4){
+                    System.out.println("Resultado: " + ejecuteOption(operacion, consola));
+                }else if(operacion == 5){
+                    System.err.println("Goodbye!");
+                    app_on = false;
+                }else{
+                    System.out.println("Opcion Erronea");
+                }
+            }catch(Exception e){
+                System.out.println("Ocurrio un error!" + e.getMessage());
+            }
+            
+            System.out.println();
+        }
         consola.close();
     }
+
+    private static void showMenu() {
+        System.out.println("***** Calculator App *****");
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicar
+                4. Dividir
+                5. Salir
+                """);
+        System.out.print("Operacion a realizar? ");
+    }
+
+    private static double ejecuteOption(int operacion, Scanner consola){
+        System.out.print("First value: ");
+        var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Second value: ");
+        var operando2 = Integer.parseInt(consola.nextLine());
+        var resultado = 0;
+        switch (operacion) {
+            case 1: //suma
+                resultado = operando1 + operando2;
+                break;
+            case 2:
+                resultado = operando1 - operando2;
+                break;
+            case 3:
+                resultado = operando1 * operando2;
+                break;                    
+            case 4:
+                resultado = operando1 / operando2;
+                break;
+            default:
+                System.out.println("Opcion Erronea");
+                break;
+        }
+        return resultado;
+    }
+    
 }
